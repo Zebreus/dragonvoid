d9e0db68d73dba7a16d12120b0219f5c390853a1
Zebreus <lennarteichhorn@googlemail.com>
2022-12-06 17:57:48 +0100

    Add nix flake

--- default.nix
+++ default.nix
@@ -5,18 +5,52 @@ pkgs.stdenv.mkDerivation {
   name = "dragonvoid";
   src = ./.;
 
-  nativeBuildInputs = [ pkgs.adoptopenjdk-jre-openj9-bin-8 ];
+  nativeBuildInputs = [ pkgs.adoptopenjdk-jre-openj9-bin-8 pkgs.makeWrapper pkgs.unzip pkgs.mktemp ];
 
   installPhase = ''
-    ls $src
     mkdir -p $out/lib
-    mkdir -p $out/usr/bin
-    cp $src/DragonVoid.jar  $out/lib
-    cat <<EOF > $out/usr/bin/dragonvoid
+    mkdir -p $out/bin
+    cp $src/DragonVoid.jar $out/lib
+
+    cat <<EOF > $out/bin/dragonvoid
     #/usr/bin/env sh
-    
-    java -cp $out/lib/DragonVoid.jar tbs.StartArenaMode
+
+    # The saves are stored inside the resource directory.
+    # We copy the resources to a temporary directory and link to a saves directory in your home folder
+    SAVE_DIR=''${SAVE_DIR-"\$HOME/.dragonvoid/"}
+    WORK_DIR=\$(${pkgs.mktemp}/bin/mktemp -d)
+    cd \$WORK_DIR 
+    ${pkgs.unzip}/bin/unzip -q $out/lib/DragonVoid.jar "res/*" -d \$WORK_DIR
+    if ! test -e "\$SAVE_DIR"
+    then
+        mkdir -p "\$SAVE_DIR"
+        cp -r \$WORK_DIR/res/saves/* "\$SAVE_DIR"
+    fi
+    rm -rf \$WORK_DIR/res/saves
+    ln -s "\$SAVE_DIR" \$WORK_DIR/res/saves
+    ${pkgs.adoptopenjdk-jre-openj9-bin-8}/bin/java -cp $out/lib/DragonVoid.jar tbs.StartMainMenu
     EOF
-    chmod 755 $out/usr/bin/dragonvoid
+
+    cat <<EOF > $out/bin/dragonvoid-arena
+    #/usr/bin/env sh
+
+    # The saves are stored inside the resource directory.
+    # We copy the resources to a temporary directory and link to a saves directory in your home folder
+    SAVE_DIR=''${SAVE_DIR-"\$HOME/.dragonvoid/"}
+    WORK_DIR=\$(${pkgs.mktemp}/bin/mktemp -d)
+    cd \$WORK_DIR 
+    ${pkgs.unzip}/bin/unzip -q $out/lib/DragonVoid.jar "res/*" -d \$WORK_DIR
+    if ! test -e "\$SAVE_DIR"
+    then
+        mkdir -p "\$SAVE_DIR"
+        cp -r \$WORK_DIR/res/saves/* "\$SAVE_DIR"
+    fi
+    rm -rf \$WORK_DIR/res/saves
+    ln -s "\$SAVE_DIR" \$WORK_DIR/res/saves
+    ${pkgs.adoptopenjdk-jre-openj9-bin-8}/bin/java -cp $out/lib/DragonVoid.jar tbs.StartArenaMode
+    EOF
+
+    chmod 755 $out/bin/dragonvoid-arena
+    chmod 755 $out/bin/dragonvoid
   '';
 }
--- flake.nix
+++ flake.nix
@@ -1,27 +1,31 @@
 {
-  # The flake identifier.
-  name = "dragonvoid";
-
-  # The epoch may be used in the future to determine how Nix
-  # expressions inside this flake are to be parsed.
-  epoch = 2022;
-
-  # Some other metadata.
   description = "An old (2017) unfinished game project from school";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        dragonvoid = (import ./. ({
+          pkgs = pkgs;
+        }));
+      in
       rec {
-        packages = flake-utils.lib.flattenTree {
-          hello = pkgs.hello;
-          gitAndTools = pkgs.gitAndTools;
+        packages.default = dragonvoid;
+
+        apps.main = flake-utils.lib.mkApp {
+          drv = dragonvoid;
+          exePath = "/bin/dragonvoid";
         };
-        defaultPackage = packages.hello;
-        apps.hello = flake-utils.lib.mkApp { drv = packages.hello; };
-        defaultApp = apps.hello;
+        apps.arena = flake-utils.lib.mkApp {
+          drv = dragonvoid;
+          exePath = "/bin/dragonvoid-arena";
+        };
+        apps.default = apps.main;
       }
     );
 }
